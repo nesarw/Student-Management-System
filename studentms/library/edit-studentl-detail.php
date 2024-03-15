52f2b9da21b947cadec9bf5a1b42e64cc3f9ae9d
Nesar Wagannawar <64674864+nesarw@users.noreply.github.com>
2024-03-15 22:19:41 +0530

    Add files via upload

---
+++ studentms/library/edit-studentl-detail.php
@@ -0,0 +1,193 @@
+<?php
+session_start();
+error_reporting(0);
+include('includes/dbconnection.php');
+if (strlen($_SESSION['sturecmsaid']==0)) {
+  header('location:logout.php');
+  } else{
+   if(isset($_POST['submit']))
+  {
+ $stuname=$_POST['stuname'];
+ $stuemail=$_POST['stuemail'];
+ $stuclass=$_POST['stuclass'];
+ $stuid=$_POST['stuid'];
+ $nobooks=$_POST['nobooks'];
+ $book1=$_POST['book1'];
+ $book2=$_POST['book2'];
+ $book3=$_POST['book3'];
+ $book4=$_POST['book4'];
+
+ $eid=$_GET['editid'];
+
+$sql="update tblstudent set StudentName=:stuname,StudentEmail=:stuemail,StudentClass=:stuclass,StuID=:stuid,nobooks=:nobooks,book1=:book1,book2=:book2,book3=:book3,book4=:book4 where ID=:eid";
+$query=$dbh->prepare($sql);
+$query->bindParam(':stuname',$stuname,PDO::PARAM_STR);
+$query->bindParam(':stuemail',$stuemail,PDO::PARAM_STR);
+$query->bindParam(':stuclass',$stuclass,PDO::PARAM_STR);
+$query->bindParam(':stuid',$stuid,PDO::PARAM_STR);
+$query->bindParam(':nobooks',$nobooks,PDO::PARAM_STR);
+$query->bindParam(':book1',$book1,PDO::PARAM_STR);
+$query->bindParam(':book2',$book2,PDO::PARAM_STR);
+$query->bindParam(':book3',$book3,PDO::PARAM_STR);
+$query->bindParam(':book4',$book4,PDO::PARAM_STR);
+$query->bindParam(':eid',$eid,PDO::PARAM_STR);
+ $query->execute();
+  echo '<script>alert("Student has been updated")</script>';
+}
+
+  ?>
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+   
+    <title>Student  Management System|| Update Students</title>
+    <!-- plugins:css -->
+    <link rel="stylesheet" href="vendors/simple-line-icons/css/simple-line-icons.css">
+    <link rel="stylesheet" href="vendors/flag-icon-css/css/flag-icon.min.css">
+    <link rel="stylesheet" href="vendors/css/vendor.bundle.base.css">
+    <!-- endinject -->
+    <!-- Plugin css for this page -->
+    <link rel="stylesheet" href="vendors/select2/select2.min.css">
+    <link rel="stylesheet" href="vendors/select2-bootstrap-theme/select2-bootstrap.min.css">
+    <!-- End plugin css for this page -->
+    <!-- inject:css -->
+    <!-- endinject -->
+    <!-- Layout styles -->
+    <link rel="stylesheet" href="css/style.css" />
+    
+  </head>
+  <body>
+    <div class="container-scroller">
+      <!-- partial:partials/_navbar.html -->
+     <?php include_once('includes/header.php');?>
+      <!-- partial -->
+      <div class="container-fluid page-body-wrapper">
+        <!-- partial:partials/_sidebar.html -->
+      <?php include_once('includes/sidebar.php');?>
+        <!-- partial -->
+        <div class="main-panel">
+          <div class="content-wrapper">
+            <div class="page-header">
+              <h3 class="page-title"> Update Students Library </h3>
+              <nav aria-label="breadcrumb">
+                <ol class="breadcrumb">
+                  <li class="breadcrumb-item"><a href="dashboard.php">Dashboard</a></li>
+                  <li class="breadcrumb-item active" aria-current="page"> Update Students Library</li>
+                </ol>
+              </nav>
+            </div>
+            <div class="row">
+          
+              <div class="col-12 grid-margin stretch-card">
+                <div class="card">
+                  <div class="card-body">
+                    <h4 class="card-title" style="text-align: center;">Update Students Library</h4>
+                   
+                    <form class="forms-sample" method="post" enctype="multipart/form-data">
+                      <?php
+$eid=$_GET['editid'];
+$sql="SELECT tblstudent.StudentName,tblstudent.StudentEmail,tblstudent.StudentClass,tblstudent.StuID,tblstudent.nobooks,tblstudent.book1,tblstudent.book2,tblstudent.book3,tblstudent.book4,tblstudent.UserName,tblstudent.Password,tblstudent.Image,tblstudent.DateofAdmission,tblclass.ClassName,tblclass.Section from tblstudent join tblclass on tblclass.ID=tblstudent.StudentClass where tblstudent.ID=:eid";
+$query = $dbh -> prepare($sql);
+$query->bindParam(':eid',$eid,PDO::PARAM_STR);
+$query->execute();
+$results=$query->fetchAll(PDO::FETCH_OBJ);
+$cnt=1;
+if($query->rowCount() > 0)
+{
+foreach($results as $row)
+{               ?>
+                      <div class="form-group">
+                        <label for="exampleInputName1">Student Name</label>
+                        <input type="text" name="stuname" value="<?php  echo htmlentities($row->StudentName);?>" class="form-control" readonly='true' required='true'>
+                      </div>
+                      <div class="form-group">
+                        <label for="exampleInputName1">Student Email</label>
+                        <input type="text" name="stuemail" value="<?php  echo htmlentities($row->StudentEmail);?>" class="form-control" readonly='true' required='true'>
+                      </div>
+                      <div class="form-group">
+                        <label for="exampleInputEmail3">Student Class</label>
+                        <select  name="stuclass" class="form-control" required='true' readonly='true'>
+                          <option value="<?php  echo htmlentities($row->StudentClass);?>"><?php  echo htmlentities($row->ClassName);?> <?php  echo htmlentities($row->Section);?></option>
+                         <?php 
+
+$sql2 = "SELECT * from    tblclass ";
+$query2 = $dbh -> prepare($sql2);
+$query2->execute();
+$result2=$query2->fetchAll(PDO::FETCH_OBJ);
+
+foreach($result2 as $row1)
+{          
+    ?>  
+<option value="<?php echo htmlentities($row1->ClassName);?><?php echo htmlentities($row1->Section);?>"><?php echo htmlentities($row1->ClassName);?> <?php echo htmlentities($row1->Section);?></option>
+ <?php } ?> 
+                        </select>
+                      <div class="form-group">
+                        <label for="exampleInputName1">Student ID</label>
+                        <input type="text" name="stuid" value="<?php  echo htmlentities($row->StuID);?>" class="form-control" readonly='true'>
+                      </div>
+                      <h3>Library details</h3>
+                      <div class="form-group">
+                        <label for="exampleInputName1">No of Books Issued (Max 4 Books)</label>
+                        <input type="text" name="nobooks" value="<?php  echo htmlentities($row->nobooks);?>" class="form-control" required='true'>
+                      </div>
+                      <div class="form-group">
+                        <label for="exampleInputName1">Name - Book 1</label>
+                        <input type="text" name="book1" value="<?php  echo htmlentities($row->book1);?>" class="form-control" required='true'>
+                      </div>
+                      <div class="form-group">
+                        <label for="exampleInputName1">Name - Book 2</label>
+                        <input type="text" name="book2" value="<?php  echo htmlentities($row->book2);?>" class="form-control" required='true'>
+                      </div>
+                      <div class="form-group">
+                        <label for="exampleInputName1">Name - Book 3</label>
+                        <input type="text" name="book3" value="<?php  echo htmlentities($row->book3);?>" class="form-control" required='true'>
+                      </div>
+                      <div class="form-group">
+                        <label for="exampleInputName1">Name - Book 4</label>
+                        <input type="text" name="book4" value="<?php  echo htmlentities($row->book4);?>" class="form-control" required='true'>
+                      </div>
+
+                      <!-- <h3>Login details</h3>
+                      <div class="form-group">
+                        <label for="exampleInputName1">User Name</label>
+                        <input type="text" name="uname" value="<?php  echo htmlentities($row->UserName);?>" class="form-control" readonly='true'>
+                      </div>
+                      <div class="form-group">
+                        <label for="exampleInputName1">Password</label>
+                        <input type="Password" name="password" value="<?php  echo htmlentities($row->Password);?>" class="form-control" readonly='true'> -->
+                      </div><?php $cnt=$cnt+1;}} ?>
+                      <button type="submit" class="btn btn-primary mr-2" name="submit">Update</button> 
+                     
+                    </form>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <!-- content-wrapper ends -->
+          <!-- partial:partials/_footer.html -->
+         <?php include_once('includes/footer.php');?>
+          <!-- partial -->
+        </div>
+        <!-- main-panel ends -->
+      </div>
+      <!-- page-body-wrapper ends -->
+    </div>
+    <!-- container-scroller -->
+    <!-- plugins:js -->
+    <script src="vendors/js/vendor.bundle.base.js"></script>
+    <!-- endinject -->
+    <!-- Plugin js for this page -->
+    <script src="vendors/select2/select2.min.js"></script>
+    <script src="vendors/typeahead.js/typeahead.bundle.min.js"></script>
+    <!-- End plugin js for this page -->
+    <!-- inject:js -->
+    <script src="js/off-canvas.js"></script>
+    <script src="js/misc.js"></script>
+    <!-- endinject -->
+    <!-- Custom js for this page -->
+    <script src="js/typeahead.js"></script>
+    <script src="js/select2.js"></script>
+    <!-- End custom js for this page -->
+  </body>
+</html><?php }  ?>
---
+++ studentms/library/includes/sidebar.php
@@ -0,0 +1,65 @@
+<nav class="sidebar sidebar-offcanvas" id="sidebar">
+          <ul class="nav">
+            <li class="nav-item nav-profile">
+              <a href="#" class="nav-link">
+                <div class="profile-image">
+                  <img class="img-xs rounded-circle" src="images/faces/face8.jpg" alt="profile image">
+                  <div class="dot-indicator bg-success"></div>
+                </div>
+                <div class="text-wrapper">
+                  <?php
+         $aid= $_SESSION['sturecmsaid'];
+$sql="SELECT * from tbladmin where ID=:aid";
+
+$query = $dbh -> prepare($sql);
+$query->bindParam(':aid',$aid,PDO::PARAM_STR);
+$query->execute();
+$results=$query->fetchAll(PDO::FETCH_OBJ);
+
+$cnt=1;
+if($query->rowCount() > 0)
+{
+foreach($results as $row)
+{               ?>
+                  <p class="profile-name"><?php  echo htmlentities($row->AdminName);?></p>
+                  <p class="designation"><?php  echo htmlentities($row->Email);?></p><?php $cnt=$cnt+1;}} ?>
+                </div>
+               
+              </a>
+            </li>
+            <li class="nav-item nav-category">
+              <span class="nav-link">Dashboard</span>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="dashboard.php">
+                <span class="menu-title">Dashboard</span>
+                <i class="icon-screen-desktop menu-icon"></i>
+              </a>
+            </li>
+
+            <li class="nav-item">
+              <a class="nav-link" data-toggle="collapse" href="#ui-basic" aria-expanded="false" aria-controls="ui-basic">
+                <span class="menu-title">Librarian</span>
+                <i class="icon-layers menu-icon"></i>
+              </a>
+              <div class="collapse" id="ui-basic">
+                <ul class="nav flex-column sub-menu">
+                  <li class="nav-item"> <a class="nav-link" href="add-librarian.php">Add Librarian</a></li>
+                  <li class="nav-item"> <a class="nav-link" href="manage-librarian.php">Manage Librarian</a></li>
+                </ul>
+              </div>
+            </li>
+            
+            <li class="nav-item">
+              <a class="nav-link" data-toggle="collapse" href="#ui-basic1" aria-expanded="false" aria-controls="ui-basic1">
+                <span class="menu-title">Students Library <br> Details</span>
+                <i class="icon-people menu-icon"></i>
+              </a>
+              <div class="collapse" id="ui-basic1">
+                <ul class="nav flex-column sub-menu">
+                  <li class="nav-item"> <a class="nav-link" href="manage-studentsl.php">Manage Student Library <br> Details</a></li>
+                </ul>
+              </div>
+            </li>
+          </ul>
+        </nav>
